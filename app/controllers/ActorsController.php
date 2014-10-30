20866c5f59086d0b4d9f99bdb9aafe1b9c3d6553
Chris Nielsen <chn91@me.com>
2014-10-30 09:24:52 +0100

    Added Movies and Actors Controller

---
+++ app/controllers/ActorsController.php
@@ -0,0 +1,28 @@
+<?php
+
+class ActorsController extends \BaseController {
+
+    /**
+     * Display a listing of the resource.
+     * GET /movies
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     * GET /movies/{id}
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+}
--- app/views/partials/nav.blade.php
+++ app/views/partials/nav.blade.php
@@ -2,9 +2,9 @@
     <div class="container">
         <h1 class="pull-left">Watch It</h1>
         <ul>
-            <li><a href=""><span class="glyphicon glyphicon-home"></span><span class="menu-desc">Home</span></a></li>
-            <li><a href=""><span class="glyphicon glyphicon-film"></span><span class="menu-desc">Movies</span></a></li>
-            <li><a href=""><span class="glyphicon glyphicon-user"></span><span class="menu-desc">Actors</span></a></li>
+            <li><a href="/"><span class="glyphicon glyphicon-home"></span><span class="menu-desc">Home</span></a></li>
+            <li><a href="/movie"><span class="glyphicon glyphicon-film"></span><span class="menu-desc">Movies</span></a></li>
+            <li><a href="/actor"><span class="glyphicon glyphicon-user"></span><span class="menu-desc">Actors</span></a></li>
         </ul>
     </div>
 </nav>
